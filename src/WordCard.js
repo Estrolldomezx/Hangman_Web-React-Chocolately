01bcc4bd643ada9c41698da640effd9dac7d30f8
Wanish Sengkhim <wanish45037.2@gmail.com>
2020-08-15 05:09:12 +0700

    Part 7.4

--- src/WordCard.js
+++ src/WordCard.js
@@ -44,7 +44,7 @@ class App extends React.Component {
     let resetevery= this.state.completed === true ? <button onClick={this.reset}><h1>Play Again</h1></button>: '';    
 
     return (
-     <div><h1 class="R2">Find the world</h1>
+     <div><h1 class="R2">Find the word</h1>
       <div className="Backgrounf">
        
         {Array.from(this.state.chars).map((item, index) => <CharacterCard value={item} key={index}  attempt={this.state.attempt} activationHandler={this.activationHandler} />)}
